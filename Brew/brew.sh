c614c7e9960c60b2c081263ee5289dfae816125a
ricardo <ricardo.martinez@bq.com>
2016-01-03 12:16:49 +0100

    homebrew script added

---
+++ Brew/brew.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+# Install binary packages using Homebrew (http://brew.sh)
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# Make sure we're using the latest Homebrew
+brew update
+
+# Install wget with IRI support
+brew install wget --with-iri
+
+# Install more recent version of vim
+brew install vim --with-lua --override-system-vi
+
+# Install everything else
+brew install brew-cask
+brew install macvim
+brew install ruby
+brew install phantomjs
+brew install mongodb
+brew install maven
+brew install openssl
+brew install cscope
+brew install ctags
+brew install python3
+
+
+# Remove outdated versions from the cellar
+brew cleanup
+
---
+++ Brew/cask.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+
+# Install apps using Homebrew-Cask (http://caskroom.io/)
+
+# Ask for the administrator password upfront.
+sudo -v
+
+# Keep-alive: update existing `sudo` time stamp until `cask.sh` has finished.
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
+# Update Homebrew to ensure we are using the latest Homebrew-cask version
+brew update
+
+# Install Java 6 required by some apps
+brew cask install java
+
+
+# Install applications
+brew cask install macvim
+brew cask install robomongo
+brew cask install smartgit
+brew cask install telegram
+brew cask install unrarx
+brew cask install google-chrome
+brew cask install intellij-idea-ce
+brew cask install spotify
+brew cask install tuxera-ntfs
+brew cask install firefox
+brew cask install android-studio
+brew cask install android-sdk
+brew cask install sublime-text3
+brew cask install vlc
+brew cask install virtualbox
+brew cask install slack
+brew cask install cleanmymac
+brew cask install libreoffice
+
+# Remove outdated versions from the cellar
+brew cleanup
+brew cask cleanup
+
